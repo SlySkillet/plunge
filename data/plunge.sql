c368d548ccfaedcef67645c2472d5e5653cda67d
hnrykm <hnrykm@gmail.com>
2023-06-28 21:06:22 -0400

    plunge.sql schema completed

--- data/plunge.sql
+++ data/plunge.sql
@@ -1,51 +1,75 @@
+DROP TABLE IF EXISTS locations;
+DROP TABLE IF EXISTS categories;
 DROP TABLE IF EXISTS accounts;
+DROP TABLE IF EXISTS account_details;
 DROP TABLE IF EXISTS events;
+DROP TABLE IF EXISTS classes;
+DROP TABLE IF EXISTS reservations;
 
 CREATE TABLE locations (
     id SERIAL NOT NULL UNIQUE,
-    name VARCHAR NOT NULL,
-    address VARCHAR NOT NULL,
-    city VARCHAR NOT NULL,
-    state VARCHAR NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    address VARCHAR(100) NOT NULL,
+    city VARCHAR(50) NOT NULL,
+    state VARCHAR(50) NOT NULL,
     zip_code SMALLINT NOT NULL,
-    latitude VARCHAR,
-    longitude VARCHAR
-)
+    latitude VARCHAR(100),
+    longitude VARCHAR(100)
+);
 
 CREATE TABLE categories (
     id SERIAL NOT NULL UNIQUE,
-    name VARCHAR NOT NULL
-)
-
+    name VARCHAR(100) NOT NULL
+);
 
 CREATE TABLE accounts (
     id SERIAL NOT NULL UNIQUE,
-    username VARCHAR NOT NULL,
-    first_name VARCHAR NOT NULL,
-    last_name VARCHAR NOT NULL,
-    email VARCHAR NOT NULL,
-    password VARCHAR NOT NULL,
-)
+    username VARCHAR(100) NOT NULL,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL
+);
 
--- CREATE TABLE account_details (
---     id SERIAL NOT NULL UNIQUE,
---     account_id INTEGER REFERENCES accounts("id") ON DELETE CASCADE,
---     avatar VARCHAR,
---     phone_number INTEGER,
---     biography TEXT,
---     mock_credit_card VARCHAR,
---     interests
+CREATE TABLE account_details (
+    id SERIAL NOT NULL UNIQUE,
+    account_id INTEGER REFERENCES accounts("id") ON DELETE CASCADE,
+    avatar VARCHAR(100),
+    phone_number INTEGER,
+    biography TEXT,
+    mock_credit_card VARCHAR(100),
+    interests INTEGER REFERENCES categories("id") ON DELETE CASCADE,
+    location_id SMALLINT REFERENCES locations("id") ON DELETE CASCADE
+);
 
--- )
+CREATE TABLE classes (
+    id SERIAL NOT NULL UNIQUE,
+    class_name VARCHAR(100) NOT NULL,
+    instructor_id INTEGER REFERENCES accounts("id") ON DELETE CASCADE,
+    requirements TEXT NOT NULL,
+    category_id SMALLINT NOT NULL,
+    description TEXT NOT NULL,
+    price SMALLINT NOT NULL,
+    featured BOOLEAN NOT NULL,
+    image_1 VARCHAR(100) NOT NULL,
+    image_2 VARCHAR(100),
+    image_3 VARCHAR(100),
+    image_4 VARCHAR(100),
+    location_id SMALLINT REFERENCES locations("id") ON DELETE CASCADE
+);
 
--- CREATE TABLE events (
---     id SERIAL NOT NULL UNIQUE,
---     date_time TIMESTAMP NOT NULL,
---     capacity SMALLINT NOT NULL,
---     class_id INT REFERENCES classes("id") ON DELETE CASCADE
--- )
+CREATE TABLE events (
+    id SERIAL NOT NULL UNIQUE,
+    date_time TIMESTAMP NOT NULL,
+    capacity SMALLINT NOT NULL,
+    class_id INTEGER REFERENCES classes("id") ON DELETE CASCADE
+);
 
--- CREATE TABLE classes (
---     id SERIAL NOT NULL UNIQUE,
-
--- )
+CREATE TABLE reservations (
+    id SERIAL NOT NULL UNIQUE,
+    total_price NUMERIC NOT NULL,
+    status BOOLEAN NOT NULL DEFAULT true,
+    event_id SMALLINT REFERENCES events("id") ON DELETE CASCADE,
+    class_id SMALLINT REFERENCES classes("id") ON DELETE CASCADE,
+    student_id SMALLINT REFERENCES accounts("id") ON DELETE CASCADE
+);
---
+++ journals/henry_kim.md
@@ -0,0 +1,11 @@
+## June 28, 2023
+
+Today, I worked on:
+
+- Setting up our Plunge project with our team.
+- Adding to docker-compose.yml - volumes, postgres setup, pg-admin
+- Completed designing the project database structure
+- Converted from migrations-based to .sql-based setup
+- Completed plunge.sql schema
+
+Team collaboration using screen share to complete the list of tasks above. First experience working on coding as a team - great input from Greg and Simon. Followed instructions from Learn to setup the docker-compose.yml file to set up PostgreSQL. Used notes and references to create .sql file. Made a silly mistake with Git file not updating. Struggled to decide to separate locations from classes and account_details tables. Used diagram from drawsql.app to complete plunge.sql schema.
