5668c6e216e0708f0877984fe3f03ba9e4a5fc7c
Gregory Herren <greg.herren@gmail.com>
2023-07-12 18:20:09 +0000

    LUC-97 Include location details and category name in /classes GET one and GET list

--- api/queries/classes.py
+++ api/queries/classes.py
@@ -1,13 +1,11 @@
 from pydantic import BaseModel
 from queries.pool import pool
-from typing import (
-    Union,
-    List,
-    Optional
-)
+from typing import Union, List, Optional
+
 
 class Error(BaseModel):
     message: str
+
 
 class ClassIn(BaseModel):
     class_name: str
@@ -22,6 +20,7 @@ class ClassIn(BaseModel):
     image_3: str
     image_4: str
     location_id: int
+
 
 class ClassOut(BaseModel):
     id: int
@@ -38,18 +37,31 @@ class ClassOut(BaseModel):
     image_4: str
     location_id: int
 
+
+class ClassOutDetail(ClassOut):
+    category_name: str
+    location_name: str
+    location_address: str
+    location_city: str
+    location_state: str
+    location_zip_code: str
+    location_latitude: str = None
+    location_longitude: str = None
+
+
 class ClassQueries(BaseModel):
-    def get_all(self) -> Union[Error, List[ClassOut]]:
+    def get_all(self) -> Union[Error, List[ClassOutDetail]]:
         try:
             with pool.connection() as conn:
                 with conn.cursor() as db:
                     db.execute(
                         """
-                        SELECT id
+                        SELECT classes.id
                             , class_name
                             , instructor_id
                             , requirements
                             , category_id
+                            , categories.name
                             , description
                             , price
                             , featured
@@ -58,32 +70,27 @@ class ClassQueries(BaseModel):
                             , image_3
                             , image_4
                             , location_id
+                            , locations.name
+                            , locations.address
+                            , locations.city
+                            , locations.state
+                            , locations.zip_code
+                            , locations.latitude
+                            , locations.longitude
                         FROM classes
+                        INNER JOIN categories on classes.category_id = categories.id
+                        INNER JOIN locations on classes.location_id = locations.id
                         """
                     )
                     return [
-                        ClassOut(
-                            id=record[0],
-                            class_name=record[1],
-                            instructor_id = record[2],
-                            requirements=record[3],
-                            category_id=record[4],
-                            description=record[5],
-                            price=record[6],
-                            featured=record[7],
-                            image_1 = record[8],
-                            image_2=record[9],
-                            image_3=record[10],
-                            image_4=record[11],
-                            location_id=record[12],
-                        )
+                        self.record_to_class_detail_out(record)
                         for record in db
                     ]
         except Exception as e:
             print(e)
             return {"message": "could not get all classes"}
 
-    def create(self, class_info: ClassIn) -> Union[ClassIn, Error]:
+    def create(self, class_info: ClassIn) -> Union[ClassOut, Error]:
         try:
             with pool.connection() as conn:
                 with conn.cursor() as db:
@@ -120,30 +127,31 @@ class ClassQueries(BaseModel):
                             class_info.image_2,
                             class_info.image_3,
                             class_info.image_4,
-                            class_info.location_id
-                        ]
+                            class_info.location_id,
+                        ],
                     )
                     id = result.fetchone()[0]
                     return self.class_in_to_out(id, class_info)
         except Exception as e:
-                print(e)
-                return {"message": "create didn't work"}
+            print(e)
+            return {"message": "create didn't work"}
 
-    def class_in_to_out(self, id:int, class_info: ClassIn):
+    def class_in_to_out(self, id: int, class_info: ClassIn):
         old_data = class_info.dict()
         return ClassOut(id=id, **old_data)
 
-    def get_one(self, class_id: int) -> Optional[ClassOut]:
+    def get_one(self, class_id: int) -> Optional[ClassOutDetail]:
         try:
             with pool.connection() as conn:
                 with conn.cursor() as db:
                     result = db.execute(
                         """
-                        SELECT id
+                        SELECT classes.id
                             , class_name
                             , instructor_id
                             , requirements
                             , category_id
+                            , categories.name
                             , description
                             , price
                             , featured
@@ -152,29 +160,49 @@ class ClassQueries(BaseModel):
                             , image_3
                             , image_4
                             , location_id
+                            , locations.name
+                            , locations.address
+                            , locations.city
+                            , locations.state
+                            , locations.zip_code
+                            , locations.latitude
+                            , locations.longitude
                         FROM classes
-                        WHERE id = %s
+                        INNER JOIN categories on classes.category_id = categories.id
+                        INNER JOIN locations on classes.location_id = locations.id
+                        WHERE classes.id = %s
                         """,
                         [class_id],
                     )
                     record = result.fetchone()
                     if record is None:
                         return None
-                    return ClassOut(
-                        id=record[0],
-                        class_name=record[1],
-                        instructor_id = record[2],
-                        requirements=record[3],
-                        category_id=record[4],
-                        description=record[5],
-                        price=record[6],
-                        featured=record[7],
-                        image_1 = record[8],
-                        image_2=record[9],
-                        image_3=record[10],
-                        image_4=record[11],
-                        location_id=record[12],
-                    )
+                    return self.record_to_class_detail_out(record)
         except Exception as e:
             print(e)
             return {"message": "could not get that class info"}
+
+    def record_to_class_detail_out(self, record):
+        return ClassOutDetail(
+            id=record[0],
+            class_name=record[1],
+            instructor_id=record[2],
+            requirements=record[3],
+            category_id=record[4],
+            category_name=record[5],
+            description=record[6],
+            price=record[7],
+            featured=record[8],
+            image_1=record[9],
+            image_2=record[10],
+            image_3=record[11],
+            image_4=record[12],
+            location_id=record[13],
+            location_name=record[14],
+            location_address=record[15],
+            location_city=record[16],
+            location_state=record[17],
+            location_zip_code=record[18],
+            location_latitude=record[19],
+            location_longitude=record[20],
+        )
