d2ade6ff0d496a991c14815ba8a2f7a714e0d91d
Simon Conrad <simondbconrad@gmail.com>
2023-07-17 22:35:48 +0000

    Footer LUC-102

---
+++ ghi/src/Footer.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="footer navbar navbar-expand-lg navbar-light bg-light">
+      <div></div>
+      <div className="footer-item">
+        <p>Prouced By:</p>
+        <ul>
+          <li>Simon Conrad</li>
+          <li>Travis Semeniv</li>
+          <li>Greg Herren</li>
+          <li>Henry Kim</li>
+        </ul>
+      </div>
+      <div></div>
+      <div className="footer-item">
+        <p>Thanks for visiting Plunged!</p>
+      </div>
+      <div></div>
+    </div>
+  );
+}
+
+export default Footer;
