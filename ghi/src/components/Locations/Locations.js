0fa2f76066add0bb7f813c8e8f275626b409071b
Simon Conrad <simondbconrad@gmail.com>
2023-07-19 15:30:29 -0400

    create useEffect and useState to store upcoming classes

--- ghi/src/components/Locations/Locations.js
+++ ghi/src/components/Locations/Locations.js
@@ -1,12 +1,29 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useState, useEffect } from "react";
 import { GoogleMap, useLoadScript, MarkerF } from "@react-google-maps/api";
 
 function Map() {
   const center = useMemo(() => ({ lat: 38.909677, lng: -77.029657 }), []);
-  console.log("center =>", center);
+  const [classes, setClasses] = useState([]);
+  useEffect(() => {
+    async function loadClasses() {
+      const response = await fetch(
+        "http://localhost:8000/classes?feed=upcoming"
+      );
+      if (response.ok) {
+        const data = await response.json();
+        setClasses(data);
+      } else {
+        console.error(response);
+      }
+    }
+    loadClasses();
+  }, []);
+  console.log("classes => ", classes);
+
   return (
     <GoogleMap zoom={15} center={center} mapContainerClassName="map-container">
       <MarkerF position={{ lat: 38.909677, lng: -77.029657 }} />
+      {/* <MarkerF position={{ lat: 38.911105, lng: -77.029666 }} /> */}
     </GoogleMap>
   );
 }
