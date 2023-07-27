6420d0e15dd6271ef38ac8ee7c69cddba93d24b8
Simon Conrad <simondbconrad@gmail.com>
2023-07-27 17:02:02 +0000

    Category page LUC-118

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,66 +1,66 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 // import Construct from "./Construct.js";
 // import ErrorNotification from "./ErrorNotification";
-import './App.css';
-import MainPage from './components/Classes/MainPage/Main_Page';
-import { AuthProvider } from '@galvanize-inc/jwtdown-for-react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LoginForm from './LoginForm';
-import SignupForm from './SignupForm';
-import ClassDetails from './components/Classes/ClassDetails';
-import ClassesForm from './components/Classes/ClassesForm';
-import EventsForm from './components/Events/EventsForm';
-import Reservations from './components/Accounts/Reservations';
-import ProfileForm from './components/Accounts/ProfileForm';
-import Profile from './components/Accounts/Profile';
-import Dashboard from './components/Accounts/Dashboard';
-import Nav from './Nav';
-import Footer from './Footer';
-import UpcomingClasses from './components/Classes/Classes';
-import AllUpcomingClasses from './components/Classes/AllUpcomingClasses';
-import BrowseCategories from './components/Categories/Categories';
-import Category from './components/Categories/Category';
+import "./App.css";
+import MainPage from "./components/Classes/MainPage/Main_Page";
+import { AuthProvider } from "@galvanize-inc/jwtdown-for-react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginForm from "./LoginForm";
+import SignupForm from "./SignupForm";
+import ClassDetails from "./components/Classes/ClassDetails";
+import ClassesForm from "./components/Classes/ClassesForm";
+import EventsForm from "./components/Events/EventsForm";
+import Reservations from "./components/Accounts/Reservations";
+import ProfileForm from "./components/Accounts/ProfileForm";
+import Profile from "./components/Accounts/Profile";
+import Dashboard from "./components/Accounts/Dashboard";
+import Nav from "./Nav";
+import Footer from "./Footer";
+import UpcomingClasses from "./components/Classes/Classes";
+import AllUpcomingClasses from "./components/Classes/AllUpcomingClasses";
+import BrowseCategories from "./components/Categories/Categories";
+import Category from "./components/Categories/Category";
 
 function App() {
-	const baseUrl = process.env.REACT_APP_SAMPLE_SERVICE_API_HOST;
-	const [launchInfo, setLaunchInfo] = useState([]);
-	const [error, setError] = useState(null);
+  const baseUrl = process.env.REACT_APP_SAMPLE_SERVICE_API_HOST;
+  const [launchInfo, setLaunchInfo] = useState([]);
+  const [error, setError] = useState(null);
 
-	return (
-		<AuthProvider baseUrl={baseUrl}>
-			<BrowserRouter>
-				<Nav />
-				<div className="container">
-					<Routes>
-						<Route path="/" element={<MainPage />} />
+  return (
+    <AuthProvider baseUrl={baseUrl}>
+      <BrowserRouter>
+        <Nav />
+        <div className="container">
+          <Routes>
             <Route path="/" element={<MainPage />} />
-						<Route path="/login" element={<LoginForm />} />
-						<Route path="/signup" element={<SignupForm />} />
-						<Route path="/classes/create" element={<ClassesForm />} />
-						<Route path="/classes/:classId/edit" element={<ClassesForm />} />
-						<Route path="/classes/:classId" element={<ClassDetails />} />
-						<Route path="/dashboard" element={<Dashboard />} />
-						<Route
-							path="/classes/:classId/events/create"
-							element={<EventsForm />}
-						/>
-						<Route
-							path="/classes/:classId/events/:eventId/edit"
-							element={<EventsForm />}
-						/>
-						<Route path="/reservations" element={<Reservations />} />
-						<Route path="/profile/edit" element={<ProfileForm />} />
-						<Route path="/profile" element={<Profile />} />
-						<Route path="/upcoming" element={<UpcomingClasses />} />
-						<Route path="/all-upcoming" element={<AllUpcomingClasses />} />
-						<Route path="/browse-categories" element={<BrowseCategories />} />
-						<Route path="categories/:Id" elemet={<Category />} />
-					</Routes>
-				</div>
-				<Footer />
-			</BrowserRouter>
-		</AuthProvider>
-	);
+            <Route path="/" element={<MainPage />} />
+            <Route path="/login" element={<LoginForm />} />
+            <Route path="/signup" element={<SignupForm />} />
+            <Route path="/classes/create" element={<ClassesForm />} />
+            <Route path="/classes/:classId/edit" element={<ClassesForm />} />
+            <Route path="/classes/:classId" element={<ClassDetails />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route
+              path="/classes/:classId/events/create"
+              element={<EventsForm />}
+            />
+            <Route
+              path="/classes/:classId/events/:eventId/edit"
+              element={<EventsForm />}
+            />
+            <Route path="/reservations" element={<Reservations />} />
+            <Route path="/profile/edit" element={<ProfileForm />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/upcoming" element={<UpcomingClasses />} />
+            <Route path="/all-upcoming" element={<AllUpcomingClasses />} />
+            <Route path="/browse-categories" element={<BrowseCategories />} />
+            <Route path="categories/:Id" element={<Category />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </AuthProvider>
+  );
 }
 
 export default App;
--- ghi/src/components/Categories/Category.js
+++ ghi/src/components/Categories/Category.js
@@ -1,8 +1,83 @@
-import { React } from 'react';
-import { useParams } from 'react-router-dom';
+import { React, useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 function Category() {
-	return <div>Grid of classes by category here...</div>;
+  let { Id } = useParams();
+  const [classes, setClasses] = useState([]);
+
+  const fetchData = async () => {
+    let url = `http://localhost:8000/classes?category=${Id}`;
+    let response = await fetch(url);
+    if (response.ok) {
+      let data = await response.json();
+      setClasses(data);
+    } else {
+      console.error(response);
+    }
+  };
+
+  const categoriesTable = {
+    1: "Music",
+    2: "Design & Style",
+    3: "Arts & Entertainment",
+    4: "Business",
+    5: "Sports & Gaming",
+    6: "Writing",
+    7: "Science & Tech",
+    8: "Home & Lifestyle",
+    9: "Community & Government",
+    10: "Health & Wellness",
+    11: "Food",
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  console.log("classes =>", classes);
+  return (
+    <div className="all-upcoming-card-container">
+      <h1 className="upcoming-title">{categoriesTable[Id]}</h1>
+      <div className="card-container">
+        {classes.map((classIterable, idx) => {
+          const classDetailUrl = `/classes/${classIterable.id}`;
+          return (
+            <div className="card location-card mx-2" key={idx}>
+              <div className="card-body location-card">
+                <img
+                  src={classIterable.image_1}
+                  className="card-img-top"
+                  alt="..."
+                />
+                <h5 className="card-title location-card">
+                  {classIterable.class_name}
+                </h5>
+                <div></div>
+                <p className="card-text location-card">
+                  {classIterable.description.length > 143
+                    ? classIterable.description.substr(0, 140) + "..."
+                    : classIterable.description}
+                </p>
+                <ul className="list-group list-group-flush location-card">
+                  <li className="list-group-item">
+                    <p className="card-text location-card">
+                      {classIterable.location_address.length > 16
+                        ? classIterable.location_address.substr(0, 13) + "..."
+                        : classIterable.location_address}
+                    </p>
+                  </li>
+                  <li className="list-group-item">
+                    <a href={classDetailUrl} className="btn btn-primary">
+                      Class Details
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Category;
